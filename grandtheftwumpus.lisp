a972779853dc5b91f589ff3182f9fe7105c03f6b
David Clark <david@Augustine.(none)>
2012-12-15 14:27:40 -0600

    Finished analyzing wumpus game

--- grandtheftwumpus.lisp
+++ grandtheftwumpus.lisp
@@ -257,3 +257,23 @@
 		      (princ new-pos)
 		      (handle-new-place nil new-pos nil))))))
 	  
+
+;;faster connected functions from chapter 9
+(defun hash-edges (edge-list)
+  (let ((tab (make-hash-table)))
+    (mapc (lambda (x)
+	    (let ((node (car x)))
+	      (push (cdr x) (gethash node tab))))
+	  edge-list)
+    tab))
+
+(defun get-connected-hash (node edge-tab)
+  (let ((visited (make-hash-table)))
+    (labels ((traverse (node)
+	       (unless (gethash node visited)
+		 (setf (gethash node visited) t)
+		 (mapc (lambda (edge)
+			 (traverse edge))
+		       (gethash node edge-tab)))))
+      (traverse node))
+    visited))
